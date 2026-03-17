084dc04374bd7baefb7972b262da9325c681e807
Olivier Cazade <ocazade@redhat.com>
2026-03-17 16:10:58 +0100

    Update API dependency with new featuregate

--- vendor/github.com/openshift/api/features/features.go
+++ vendor/github.com/openshift/api/features/features.go
@@ -1033,4 +1033,12 @@ var (
 					enhancementPR("https://github.com/openshift/enhancements/pull/1933").
 					enable(inDevPreviewNoUpgrade(), inTechPreviewNoUpgrade()).
 					mustRegister()
+
+	FeatureGateNetworkObservabilityInstall = newFeatureGate("NetworkObservabilityInstall").
+						reportProblemsToJiraComponent("netobserv").
+						contactPerson("jtakvori").
+						productScope(ocpSpecific).
+						enhancementPR("https://github.com/openshift/enhancements/pull/1908").
+						enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
+						mustRegister()
 )
